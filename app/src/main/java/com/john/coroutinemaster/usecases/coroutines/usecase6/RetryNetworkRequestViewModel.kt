234c63806935828be7c25e8412b3da8b5b897aca
john.zar <melow2@naver.com>
2022-05-08 13:25:00 +0900

    [개발] request 반복 뷰모델 작성.

--- app/src/main/java/com/john/coroutinemaster/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
+++ app/src/main/java/com/john/coroutinemaster/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
@@ -1,14 +1,36 @@
 package com.john.coroutinemaster.usecases.coroutines.usecase6
 
+import androidx.lifecycle.viewModelScope
 import com.john.coroutinemaster.base.BaseViewModel
 import com.john.coroutinemaster.mock.MockApi
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import java.lang.Exception
 
 class RetryNetworkRequestViewModel(
     private val api: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequest() {
+        uiState.value = UiState.Loading
 
+        viewModelScope.launch {
+            val numberOfRetries = 2
+            repeat(numberOfRetries){
+                try {
+                    loadRecentAndroidVersion()
+                    return@launch
+                }catch (e:Exception){
+                    Timber.e(e)
+                    uiState.value = UiState.Error("Network request failed!")
+                }
+            }
+        }
+    }
+
+    private suspend fun loadRecentAndroidVersion(){
+        val recentAndroidVersions = api.getRecentAndroidVersions()
+        uiState.value = UiState.Success(recentAndroidVersions)
     }
 
 }
